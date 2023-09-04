acf69d6771a228ea84b957b2c68cf0a2a388bc89
CSParadiso <cayetanosimonparadiso@protonmail.com>
2023-09-04 01:42:23 -0300

    refactor: encapsulamiento de funciones

--- js/main.js
+++ js/main.js
@@ -1,120 +1,110 @@
-// Acceder al archivo subido una vez que se
-// clikea el botón "Procesar Archivo"
-
-document.getElementById("manejadorArchivos").addEventListener("submit", function (e) {
+// Acceder al archivo subido por el usuario
+document.getElementById("processFile").addEventListener("click", function (e) {
   e.preventDefault(); // Evitar que la página se actualice luego del submit
-  // console.log("Botón Apretado");
-
   // Obtener el archivo subido (no aceptamos más de uno)
   const archivoSeleccionado = document.getElementById("inputFile").files[0];
-
-
-  // Si el archivo existe, lo procesamos
-  if (archivoSeleccionado) {
-    procesarArchivo(archivoSeleccionado);
-    // console.log("Archivo subido");
+  if (archivoSeleccionado) { // Si el archivo existe
+    procesarArchivo(archivoSeleccionado); // se procesa
   }
 })
 
+// Procesar archivo
 function procesarArchivo(archivo) {
-  // Crear Objeto JavaScript que permite manipular archivos asincrónicamente
-  const lector = new FileReader(); 
-
-  // Une vez que el archivo carga completamente, 
-  lector.onload = function (event) {
-    // Obtener el contenido del archivo
-    const contenidoArchivo = event.target.result; // String
-
-    // Convertir el contenido del archivo a un JSON
-      // Obtener cada uno de los procesos del archivo
-    var procesos = contenidoArchivo.split("\n"); // Arreglo
-    console.log("Procesos como arreglo de texto: ", procesos);
-      // Colocar en un objeto cada campo del proceso
-    const tandaProcesos = []; // Nuevo contenedor de Objetos Proceso
-    for (var proceso of procesos) { // Por cada uno de los procesos
-      // console.log("Proceso como indice de arreglo (tipo String): ", proceso);
-      var campos = proceso.split(","); // Dividir sus campos
-      // console.log("Campos del proceso: ", campos);
-      const objetoProceso = { // Agregar sus campos al objeto
-        "name":campos[0],
-        "arriveTime":campos[1],
-        "nroRafagas":campos[2],
-        "rCpuTime":campos[3],
-        "rIoTime":campos[4],
-        "priority":campos[5]
-      };
-      tandaProcesos[procesos.indexOf(proceso)] = objetoProceso; // Agregar Proceso a Tanda
+  const lector = new FileReader(); // Permite manipular archivos asincrónicamente
+  lector.readAsText(archivo); // Leer el archivo como texto
+  lector.onload = function (event) { // Una vez que el archivo carga completamente, 
+    const contenidoArchivo = event.target.result; // Obtener contenido tipo String
+    const tandaProcesos = toJsonArray(contenidoArchivo); // Convertir contenido a JsonArray
+    if (tandaProcesos.length != 0) { // Si la tanda no está vacía, mostramos su contenido
+      const tabla = crearTabla(); 
+      agregarItemsTabla(tabla, tandaProcesos);
     }
-    
-    console.log(tandaProcesos);
-
-    // Si la tanda no está vacía
-    if (tandaProcesos.length != 0) {
-      const tablaProcesos = document.createElement("table");
-      const captionTabla = document.createElement("caption");
-      const filaTabla = document.createElement("tr");
-      const encabezadoNombre = document.createElement("th");
-      encabezadoNombre.innerHTML = "Nombre Proceso";
-      const encabezadoTiempoArribo = document.createElement("th");
-      encabezadoTiempoArribo.innerHTML = "Tiempo de arribo";
-      const encabezadoRafagasCpu = document.createElement("th");
-      encabezadoRafagasCpu.innerHTML = "Ráfagas de CPU para completarse";
-      const encabezadoDuracionRafagasCPU = document.createElement("th");
-      encabezadoDuracionRafagasCPU.innerHTML = "Duración ráfagas CPU";
-      const encabezadoDuracionRafagasIO = document.createElement("th");
-      encabezadoDuracionRafagasIO.innerHTML = "Duración ráfagas I/O";
-      const encabezadoPrioridad = document.createElement("th");
-      encabezadoPrioridad.innerHTML = "Prioridad";
-
-      captionTabla.innerHTML = "Datos obtenidos del Archivo";
-
-      tablaProcesos.appendChild(captionTabla);
-      tablaProcesos.appendChild(filaTabla);
-      filaTabla.appendChild(encabezadoNombre);
-      filaTabla.appendChild(encabezadoTiempoArribo);
-      filaTabla.appendChild(encabezadoRafagasCpu);
-      filaTabla.appendChild(encabezadoDuracionRafagasCPU);
-      filaTabla.appendChild(encabezadoDuracionRafagasIO);
-      filaTabla.appendChild(encabezadoPrioridad);
-      
-      const archivoProcesado = document.getElementById("datosTandaIngresada");
-      archivoProcesado.innerHTML = "";
-      archivoProcesado.style.display = "block";
-      archivoProcesado.appendChild(tablaProcesos);
-      
-      for (var procesito of tandaProcesos) {
-        var filaTablaDatos = document.createElement("tr");
-        var datoNombre = document.createElement("td");
-        var datoTiempoArribo = document.createElement("td");
-        var datoRafagasCPU = document.createElement("td");
-        var datoDuracionRafagasCPU = document.createElement("td");
-        var datoDuracionRafagasIO = document.createElement("td");
-        var datoPrioridad = document.createElement("td");
-
-        datoNombre.innerHTML = procesito.name;
-        datoTiempoArribo.innerHTML = procesito.arriveTime;
-        datoRafagasCPU.innerHTML = procesito.nroRafagas;
-        datoDuracionRafagasCPU.innerHTML = procesito.rCpuTime;
-        datoDuracionRafagasIO.innerHTML = procesito.rIoTime;
-        datoPrioridad.innerHTML = procesito.priority;
-
-        tablaProcesos.appendChild(filaTablaDatos);
-        filaTablaDatos.appendChild(datoNombre);
-        filaTablaDatos.appendChild(datoTiempoArribo);
-        filaTablaDatos.appendChild(datoRafagasCPU);
-        filaTablaDatos.appendChild(datoDuracionRafagasCPU);
-        filaTablaDatos.appendChild(datoDuracionRafagasIO);
-        filaTablaDatos.appendChild(datoPrioridad);
-      }
-      // var filaTabla = document.createElement("tr");
-      // var columnaTabla = document.createElement("td");
-      // var encabezadoTabla = document.createElement("th");
-      // var captionTabla = document.createElement("caption");
-    }
-
   };
+}
 
-  // Leer el archivo como texto (sinó no se puede manipular como tal)
-  lector.readAsText(archivo); 
+function toJsonArray(fileContent) {
+  // Obtener cada uno de los procesos del archivo (lineas del String)
+  var procesos = fileContent.split("\n"); // Dividir String (btener procesos)
+  const tandaProcesos = []; // Arreglo contenedor de Objetos Proceso
+  for (var proceso of procesos) { // Por cada uno de los procesos (items del arreglo) 
+    var campos = proceso.split(","); // Dividir String (obtener campos)
+    const objetoProceso = { // Agregar sus campos al objeto
+      "name":campos[0],
+      "arriveTime":campos[1],
+      "nroRafagas":campos[2],
+      "rCpuTime":campos[3],
+      "rIoTime":campos[4],
+      "priority":campos[5]
+    };
+    tandaProcesos[procesos.indexOf(proceso)] = objetoProceso; // Agregar Objeto Proceso a Tanda
+  }
+  return tandaProcesos;
+}
 
+function crearTabla() {
+  // Crear tabla, caption, separador y fila de encabezados
+  const tablaProcesos = document.createElement("table");
+  const captionTabla = document.createElement("caption");
+  captionTabla.innerHTML = "Datos obtenidos del Archivo";
+  const separador = document.createElement("hr");
+  const filaTabla = document.createElement("tr");
+  // Crear encabezados 
+  const encabezadoNombre = document.createElement("th");
+  encabezadoNombre.innerHTML = "Nombre Proceso";
+  const encabezadoTiempoArribo = document.createElement("th");
+  encabezadoTiempoArribo.innerHTML = "Tiempo de arribo";
+  const encabezadoRafagasCpu = document.createElement("th");
+  encabezadoRafagasCpu.innerHTML = "Ráfagas de CPU para completarse";
+  const encabezadoDuracionRafagasCPU = document.createElement("th");
+  encabezadoDuracionRafagasCPU.innerHTML = "Duración ráfagas CPU (Quantum)";
+  const encabezadoDuracionRafagasIO = document.createElement("th");
+  encabezadoDuracionRafagasIO.innerHTML = "Duración ráfagas I/O";
+  const encabezadoPrioridad = document.createElement("th");
+  encabezadoPrioridad.innerHTML = "Prioridad";
+  // Añadir elementos a sus respectivos contenedores en la página
+  document.getElementById("selectFile").insertBefore(separador, 
+    document.getElementById("datosTandaIngresada"));
+  tablaProcesos.appendChild(captionTabla);
+  tablaProcesos.appendChild(filaTabla);
+  filaTabla.appendChild(encabezadoNombre);
+  filaTabla.appendChild(encabezadoTiempoArribo);
+  filaTabla.appendChild(encabezadoRafagasCpu);
+  filaTabla.appendChild(encabezadoDuracionRafagasCPU);
+  filaTabla.appendChild(encabezadoDuracionRafagasIO);
+  filaTabla.appendChild(encabezadoPrioridad);
+  // Obtener contenedor fijo en HTML
+  const contenedorTabla = document.getElementById("datosTandaIngresada");
+  contenedorTabla.innerHTML = ""; // Borrar contenido antiguo
+  contenedorTabla.style.display = "block"; // Hacer visible
+  contenedorTabla.appendChild(tablaProcesos); // Añadir tabla generada
+  return tablaProcesos;
+}
+
+function agregarItemsTabla(tabla, tandaProcesos) {
+  for (var procesito of tandaProcesos) { // Por cada proceso
+    // Crear fila
+    var filaTablaDatos = document.createElement("tr");
+    // Crear columnas de campos ...  
+    var datoNombre = document.createElement("td"); 
+    var datoTiempoArribo = document.createElement("td");
+    var datoRafagasCPU = document.createElement("td");
+    var datoDuracionRafagasCPU = document.createElement("td");
+    var datoDuracionRafagasIO = document.createElement("td");
+    var datoPrioridad = document.createElement("td");
+    // Cada campo de proceso se escribe en su columna
+    datoNombre.innerHTML = procesito.name;
+    datoTiempoArribo.innerHTML = procesito.arriveTime;
+    datoRafagasCPU.innerHTML = procesito.nroRafagas;
+    datoDuracionRafagasCPU.innerHTML = procesito.rCpuTime;
+    datoDuracionRafagasIO.innerHTML = procesito.rIoTime;
+    datoPrioridad.innerHTML = procesito.priority;
+    // Añadir elementos en sus respectivos contenedores
+    tabla.appendChild(filaTablaDatos);
+    filaTablaDatos.appendChild(datoNombre);
+    filaTablaDatos.appendChild(datoTiempoArribo);
+    filaTablaDatos.appendChild(datoRafagasCPU);
+    filaTablaDatos.appendChild(datoDuracionRafagasCPU);
+    filaTablaDatos.appendChild(datoDuracionRafagasIO);
+    filaTablaDatos.appendChild(datoPrioridad);
+  };
 }
